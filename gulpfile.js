6066c298d66f732bf4790f4719ef4131192b8d02
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-03-24 13:37:29 -0400

    concat js functional

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,11 @@
 
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+
+gulp.task('scripts', () => {
+
+    return gulp.src(['./js/**/*.js'])
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./dist/'));
+
+});
