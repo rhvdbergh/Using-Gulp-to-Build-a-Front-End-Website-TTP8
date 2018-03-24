7a6caef7b63dc7202554258feb6102b499dab8b6
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-03-24 14:17:52 -0400

    gulp scripts concats, minifies to all.min.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,37 @@
 
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const minify = require('gulp-minify');
+const rename = require('gulp-rename');
+const clean = require('gulp-clean');
 
-gulp.task('scripts', () => {
+// concat js files
+gulp.task('concat-scripts', () => {
 
     return gulp.src(['./js/**/*.js'])
         .pipe(concat('all.js'))
         .pipe(gulp.dest('./dist/'));
+});
+
+// minify the js files -- waits for concat-scripts to complete
+gulp.task('minify-js', ['concat-scripts'], () => {
+
+    return gulp.src(['./dist/*.js'])
+        .pipe(minify({ ignoreFiles: ['all-min.js', 'all.min.js'] }))
+        .pipe(gulp.dest('./dist/'));
+});
+
+// rename the output file all-min.js to all.min.js
+gulp.task('rename-js', ['minify-js'], () => {
+
+    return gulp.src('./dist/all-min.js')
+        .pipe(rename('all.min.js'))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('scripts', ['rename-js'], () => {
+
+    return gulp.src(['./dist/all.js', './dist/all-min.js'])
+        .pipe(clean());
 
 });
